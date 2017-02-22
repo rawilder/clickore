1eb469f555e60ba43687bbc5c25fb18ba37ddfa3
rawilder <rawilder0506@gmail.com>
2017-02-21 23:50:19 -0600

    fixed prototype cookie as string bug

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -4,8 +4,8 @@ var main = function () {
     $("input").val("text")
 
     var FPS = 30;
-    var resources = (Cookies.get('resources') ? Cookies.get('resources') : 0);
-    var gather_rate = (Cookies.get('gather_rate') ? Cookies.get('gather_rate') : 1);
+    var resources = (Cookies.get('resources') ? parseInt(Cookies.get('resources')) : 0);
+    var gather_rate = (Cookies.get('gather_rate') ? parseInt(Cookies.get('gather_rate')) : 1);
 
     $('#mine').click(function (){
         resources += gather_rate;
