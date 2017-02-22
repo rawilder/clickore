98626fe21ca2817a8543905f3a649c75437012a8
bshafer93 <bshafer93@gmail.com>
2017-02-22 10:51:37 -0800

    Implenting Click function
    Trying to implement click function within the upgrade class. Not working
    yet, but very close.

--- prototype/js/upgradeClass.js
+++ prototype/js/upgradeClass.js
@@ -3,27 +3,44 @@ $( document ).ready(function() {
 
 
 
-function Upgrade (name,cost,multplier,gather_rate) {
+function Upgrade (name,cost,multplier,gather_rate,num_objects) {
     this.name = name;
     this.cost = cost;
     this.multplier = multplier;
     this.gather_rate = gather_rate;
+    this.num_objects = num_objects;
 
 
     this.getInfo = function() {
-        return this.cost + ' ' + this.type + ' ' + this.multplier;
+        return this.cost + ' ' + this.type + ' ' + this.multplier + ' ' + this.gather_rate;
     };
-    // Seeing if this works.
+
+
+    // Create the .onClick function
+    this.clicked = function() {
+         
+         resources = Cookies.get('resources');
+         console.log(resources);
+
+         if (resources >= this.cost) {
+            resources -= this.cost;
+            Cookies.set('resources', resources); 
+            gather_rate = parseInt(Cookies.get('gather_rate'));
+            gather_rate += this.gather_rate;
+            Cookies.set('gather_rate', gather_rate);     
+        }
+    };
+
     this.button = function() {
-        return '<button id="buy-' + this.name + '" class = "upgradeButton" >Buy ' + this.name + ' (' + this.cost + ')' + '</button></br>'; //.format function???
+        return '<button id="buy-' + this.name + '" class = "upgradeButton" >Buy ' + this.name + ' (' + this.cost + ')' + '</button></br>'; 
     };
 }
 // Array to contain all the games Upgrades
 var upgradeList = [];
-var minerU = new Upgrade("Miner",5,.2,2); 
+var minerU = new Upgrade("Miner",5,.2,2,null); 
 // Push each upgrade to array
 upgradeList.push(minerU); 
-var pickaxeU = new Upgrade("Pickaxe",5,.1,1);
+var pickaxeU = new Upgrade("Pickaxe",5,.1,1,null);
 upgradeList.push(pickaxeU);
 
 
@@ -33,9 +50,11 @@ var lengthU = upgradeList.length;
 for(var i = 0; i < lengthU; i++){
 
     upgradeName = upgradeList[i].name;
-    buttonU = upgradeList[i].button()
+    buttonU = upgradeList[i].button();
+    buttonID = "#buy-" + upgradeName;
+    console.log(buttonID);
     $("#Upgrade-Shop").append(upgradeList[i].button());
-
+    $('body').on('click', buttonID, upgradeList[i].clicked);
 }
 
 });
